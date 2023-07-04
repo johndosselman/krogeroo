34c0d12488ed25e97139b8102fef7f218a442343
John Dosselman <johndosselman@gmail.com>
2023-07-03 23:01:39 -0500

    Add list path

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,6 +14,10 @@ const router = createBrowserRouter([
         path: "",
         element: <Home />,
       },
+      {
+        path: "lists/:listId",
+        // TODO: add list element
+      },
     ],
   },
 ]);
